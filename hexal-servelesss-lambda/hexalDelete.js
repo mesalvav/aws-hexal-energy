53bf8dd3ebb98781fd23fb34e39c53f5391fe18b
mario salvatierra <msalv006@fiu.edu>
2019-08-22 14:05:33 -0400

    first commit first Lambda functions

---
+++ hexal-servelesss-lambda/hexalDelete.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+AWS.config.update({region: "us-east-2"});
+
+exports.handler = async (event, context) => {
+    
+  const documentClient  = new AWS.DynamoDB.DocumentClient({region: "us-east-2"});
+  let responseBody = "";
+  let statusCode = 0;
+  
+  const params = {
+    TableName: "Products",
+    Item: { id:  "12345", productname: "Solar panels"}
+   };
+  
+    try {
+      const data = await documentClient.put(params).promise();
+      responseBody = JSON.stringify(data);
+      statusCode = 201;
+    } catch (error) {
+      responseBody = `unable to put product: ${error}`;
+      statusCode = 403;
+    }
+    const response = { 
+      statusCode: statusCode,
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: responseBody
+    };
+
+    return response;
+};
